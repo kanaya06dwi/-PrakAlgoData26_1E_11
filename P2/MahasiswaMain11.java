0df2abdec75076ce88c5a85a397da12ee63509af
kanaya06dwi <kanayadwiyuniarahmawati@gmail.com>
2026-02-20 13:49:43 +0700

    2.2 Percobaan 2

---
+++ P2/MahasiswaMain11.java
@@ -0,0 +1,16 @@
+package P2;
+
+public class MahasiswaMain11 {
+    public static void main(String[] args) {
+        Mahasiswa11 mhs1 = new Mahasiswa11();
+        mhs1.nama11 = "Kanaya Dwi Yunia Rahmawati";
+        mhs1.nim11 = "254107020028";
+        mhs1.kelas11 = "TI 1E";
+        mhs1.ipk11 = 3.40;
+
+        mhs1.tampilkanInformasi11();
+        mhs1.ubahKelas11("TI 1F");
+        mhs1.updateIPK11(3.60);
+        mhs1.tampilkanInformasi11();
+    }
+}
