52ac502664b2dbd32d06f43752ba2e7a0a2ce3fc
kanaya06dwi <kanayadwiyuniarahmawati@gmail.com>
2026-02-20 13:54:46 +0700

    2.2 Percobaan 2

--- P2/MahasiswaMain11.java
+++ P2/MahasiswaMain11.java
@@ -12,8 +12,6 @@ public class MahasiswaMain11 {
         mhs1.ubahKelas11("TI 1F");
         mhs1.updateIPK11(3.60);
         mhs1.tampilkanInformasi11();
-         Mahasiswa11 mhs2 = new Mahasiswa11("Heni Tri", "254107020029", 3.50, "TI 2P");
-    mhs2.updateIPK11(3.70);
-    mhs2.tampilkanInformasi11();
+        
     }
 }
