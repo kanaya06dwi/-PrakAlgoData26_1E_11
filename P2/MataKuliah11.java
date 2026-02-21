b57cb222e6e5000837ed5496180f0cb5a455a55f
kanaya06dwi <kanayadwiyuniarahmawati@gmail.com>
2026-02-21 19:30:40 +0700

    latihan class mahasiswa

---
+++ P2/MataKuliah11.java
@@ -0,0 +1,26 @@
+package P2;
+
+public class MataKuliah11 {
+    String kodeMK11;
+    String nama11;
+    int sks11;
+    int jumlahJam11;
+    void tampilanInformasi11(){
+        System.out.println("Nama: " + nama11);
+        System.out.println("Sks: " + sks11);
+        System.out.println("Jumlah Jam: " + jumlahJam11);
+    }
+    void ubahSks11 (int sksBaru11){
+        sks11 = sksBaru11;
+    }
+    void tambahJam11 (int jamBaru11){
+        jumlahJam11 += jamBaru11;
+    }
+    void kurangiJam11 (int jamKurang11){
+        if (jumlahJam11 - jamKurang11 >= 0){
+            jumlahJam11 -= jamKurang11;
+        } else {
+            System.out.println("Jumlah jam tidak dapat dilakukan pengurangan.");
+        }
+    }
+}
