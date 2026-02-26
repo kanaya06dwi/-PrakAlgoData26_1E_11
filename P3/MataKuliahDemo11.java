bcc6a694b727835b721bdd6ca98d6a6e58d827d8
kanaya06dwi <kanayadwiyuniarahmawati@gmail.com>
2026-02-26 09:13:00 +0700

    3.4 MataKuliahDemo

---
+++ P3/MataKuliahDemo11.java
@@ -0,0 +1,29 @@
+package P3;
+import java.util.Scanner;
+public class MataKuliahDemo11 {
+    public static void main(String[] args) {
+        Scanner naya = new Scanner(System.in);
+        MataKuliah11[] arrayOfMataKuliah = new MataKuliah11[3];
+        String kode, nama, dummy;
+        int sks, jumlahJam;
+
+        for (int i = 0; i<3; i++) {
+            System.out.println("Masukkan Data MataKuliah ke-" + (i + 1));
+            System.out.print("Kode       :  ");
+            kode = naya.nextLine();
+            System.out.print("Nama       :  ");
+            nama = naya.nextLine();
+            System.out.print("SKS        :  ");
+            dummy = naya.nextLine();
+            sks = Integer.parseInt(dummy);
+            System.out.print("Jumlah Jam :  ");
+            dummy = naya.nextLine();
+            jumlahJam = Integer.parseInt(dummy);
+            System.out.println("---------------------------------------------");
+
+            arrayOfMataKuliah[i] = new MataKuliah11(kode, nama, sks, jumlahJam);
+            
+        }
+    }
+    
+}
