9e6ea75a11e1186421685dbfe3f3fed6fd38565d
kanaya06dwi <kanayadwiyuniarahmawati@gmail.com>
2026-02-26 09:25:12 +0700

    3.4 Modif Soal no 4 MataKuliahDemo

--- P3/MataKuliahDemo11.java
+++ P3/MataKuliahDemo11.java
@@ -23,7 +23,16 @@ public class MataKuliahDemo11 {
 
             arrayOfMataKuliah[i] = new MataKuliah11(kode, nama, sks, jumlahJam);
             
+            }
+            for (int i = 0; i < 3; i++) {
+                System.out.println("Data MataKuliah ke-" + (i + 1));
+                System.out.println("Kode       :  " + arrayOfMataKuliah[i].kode);
+                System.out.println("Nama       :  " + arrayOfMataKuliah[i].nama);
+                System.out.println("SKS        :  " + arrayOfMataKuliah[i].sks);
+                System.out.println("Jumlah Jam :  " + arrayOfMataKuliah[i].jumlahJam);
+                System.out.println("---------------------------------------------");
+            }
         }
     }
     
-}
+
