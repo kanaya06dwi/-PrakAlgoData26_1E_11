a56a1f27f81afd4b8f25bf3b74ac5b5412a66121
kanaya06dwi <kanayadwiyuniarahmawati@gmail.com>
2026-02-26 10:03:32 +0700

    3.4 modif soal no 2

--- P3/MataKuliah11.java
+++ P3/MataKuliah11.java
@@ -5,9 +5,17 @@ public class MataKuliah11 {
     public String nama;
     public int sks;
     public int jumlahJam;
-
+ public MataKuliah11(){
+    
+ }
     public MataKuliah11(String kode, String nama, int sks, int jumlahJam) {
         this.kode = kode;
+        this.nama = nama;
+        this.sks = sks;
+        this.jumlahJam = jumlahJam;
+    }
+    public void tambahData(String kode11, String nama, int sks, int jumlahJam) {
+        this.kode = kode11;
         this.nama = nama;
         this.sks = sks;
         this.jumlahJam = jumlahJam;
