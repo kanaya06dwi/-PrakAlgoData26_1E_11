94f4b83155dcecbd9ad735ef5105b750d2bf6587
kanaya06dwi <kanayadwiyuniarahmawati@gmail.com>
2026-03-13 20:54:24 +0700

    latihan no 1 mahasiswa

---
+++ P5/Mahasiswa.java
@@ -0,0 +1,13 @@
+package P5;
+
+public class Mahasiswa {
+    String nama11;
+    int uts11;
+    int uas11;
+
+    Mahasiswa(String nama, int uts, int uas){
+        this.nama11 = nama;
+        this.uts11 = uts;
+        this.uas11 = uas;
+    }
+}
