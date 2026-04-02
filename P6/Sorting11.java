bb854cf8814b8b0582345eb27e7c7121a47cc3c0
kanaya06dwi <kanayadwiyuniarahmawati@gmail.com>
2026-04-02 12:06:29 +0700

    Praktikum 1 sorting A,B,C

--- P6/Sorting11.java
+++ P6/Sorting11.java
@@ -28,6 +28,31 @@ public class Sorting11 {
             }
             System.out.println();
         }
+        void SelectionSort(){
+            for (int i = 0; i < jumData; i++) {
+                int min = i;
+                for (int j = 0; j < jumData; j++) {
+                    if(data[j]<data[min]){
+                        min=j;
+                    }
+                }
+                int temp=data[i];
+                data[i]=data[min];
+                data[min]=temp;
+            }
+        }
+        void insertionSort(){
+            for (int i = 1; i < data.length; i++) {
+                int temp = data[i];
+                int j=i-1;
+                while (j>=0 && data[j]>temp) {
+                    data[j+1]=data[j];
+                    j--;
+                }
+                data[j+1]=temp;
+
+            }
+        }
     }
 
 
