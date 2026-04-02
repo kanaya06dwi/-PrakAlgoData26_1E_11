334d87e29bff81549c7fbcec176915c59f5608f7
kanaya06dwi <kanayadwiyuniarahmawati@gmail.com>
2026-04-02 08:33:47 +0700

    praktikum1 sorting

---
+++ P6/Sorting11.java
@@ -0,0 +1,34 @@
+package P6;
+
+public class Sorting11 {
+    int [] data;
+    int jumData;
+
+    Sorting11 (int Data[], int jmlData){
+        jumData = jmlData;
+        data = new int[jmlData];
+        for (int i = 0; i < jumData; i++) {
+            data[i] = Data[i];
+        }
+    }
+    void bubleSort(){
+        int temp=0;
+        for (int i = 0; i < jumData; i++) {
+            for (int j = 1; j < jumData-i; j++) {
+                if (data[j-1]>data[j]){
+                temp=data[j];
+                data[j]=data[j-1];
+                data[j-1]=temp;
+            }
+            }
+        }
+        }
+        void tampil(){
+            for (int i = 0; i < jumData; i++) {
+                System.out.print(data[i]+ " ");
+            }
+            System.out.println();
+        }
+    }
+
+
