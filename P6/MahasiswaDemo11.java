8b5b76fd2bbc690ec60136676736e8ffc8b8b749
kanaya06dwi <kanayadwiyuniarahmawati@gmail.com>
2026-04-06 21:17:58 +0700

    update terbaru & terakhir mahasiswa dmeo

--- P6/MahasiswaDemo11.java
+++ P6/MahasiswaDemo11.java
@@ -5,6 +5,7 @@ public class MahasiswaDemo11 {
         Scanner Naya = new Scanner(System.in);
         MahasiswaBerprestasi11 list = new MahasiswaBerprestasi11();
         for (int i = 0; i < 5; i++) {
+            System.out.println("Masukkan data mahasiswa ke-"+(i+1));
             System.out.print("Masukkan NIM: ");
             String nim = Naya.nextLine();
             System.out.print("Masukkan Nama: ");
@@ -13,10 +14,15 @@ public class MahasiswaDemo11 {
             String kelas = Naya.nextLine();
             System.out.print("Masukkan IPK: ");
             double ipk = Double.parseDouble(Naya.nextLine());
+            System.out.println("-------------------");
             Mahasiswa11 m = new Mahasiswa11(nim, nama, kelas, ipk);
             list.tambah(m);
-            
         }
+        System.out.println("Data yang sudah terurut menggunakan SELECTION SORT (ASC)");
+        list.selectionSort();
+        list.tampil();
+        System.out.println("Data yang sudah terurut menggunakan INSERTION SORT (ASC)");
+        list.insertionSort();
+        list.tampil();
     }
-    
 }
