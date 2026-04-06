05d642982033749c1824a5ce973252990213a026
kanaya06dwi <kanayadwiyuniarahmawati@gmail.com>
2026-04-06 21:18:20 +0700

    update terbaru & terakhir mahasiswa berprestasi

--- P6/MahasiswaBerprestasi11.java
+++ P6/MahasiswaBerprestasi11.java
@@ -29,4 +29,28 @@ void bubbleSort(){
         }
     }
 }
+void selectionSort(){
+    for (int i = 0; i < listMhs.length; i++) {
+        int idxMin = i;
+        for (int j = i+1; j < listMhs.length; j++) {
+            if (listMhs[j].ipk < listMhs[idxMin].ipk) {
+                idxMin = j;
+            }
+        }
+        Mahasiswa11 tmp = listMhs[idxMin];
+        listMhs[idxMin] = listMhs[i];
+        listMhs[i] = tmp;
+    }
+}
+void insertionSort(){
+    for (int i = 1; i < listMhs.length; i++) {
+        Mahasiswa11 temp = listMhs[i];
+        int j = i;
+        while (j > 0 && listMhs[j-1].ipk < temp.ipk) {
+            listMhs[j] = listMhs[j-1];
+            j--;
+        }
+        listMhs[j] = temp;
+    }
+}
 }
