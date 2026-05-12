01d862d733da3d9282eda06ecdbf776be492e738
kanaya06dwi <kanayadwiyuniarahmawati@gmail.com>
2026-05-12 22:22:27 +0700

    2.2 Modifikasi Elemen pada Single Linked List

--- P11/SLLMain00.java
+++ P11/SLLMain00.java
@@ -1,36 +1,28 @@
 package P11;
 
-import java.util.Scanner;
-
 public class SLLMain00 {
     public static void main(String[] args) {
-        Scanner Naya = new Scanner(System.in);
-        SingleLinkedList sll = new SingleLinkedList();
-
-        for (int i = 1; i <= 3; i++) {
-            System.out.println("Data Mahasiswa ke-" + i);
-
-            System.out.print("NIM   : ");
-            String nim = Naya.nextLine();
-
-            System.out.print("Nama  : ");
-            String nama = Naya.nextLine();
-
-            System.out.print("Kelas : ");
-            String kelas = Naya.nextLine();
-
-            System.out.print("IPK   : ");
-            double ipk = Naya.nextDouble();
-            Naya.nextLine();
-
-            Mahasiswa00 mhs = new Mahasiswa00(nim, nama, kelas, ipk);
-
-            sll.addLast(mhs);
-            System.out.println("\nIsi Linked List:");
-            sll.print();
-            System.out.println();
-        }
-
-
+    SingleLinkedList sll = new SingleLinkedList();
+         Mahasiswa00 mhs1 = new Mahasiswa00("2501", "Alvaro", "1A", 4.0);
+         Mahasiswa00 mhs2 = new Mahasiswa00("2502", "Kanaya", "1E", 4.0);
+            Mahasiswa00 mhs3 = new Mahasiswa00("2503", "Zidan", "1E", 4.0);
+            Mahasiswa00 mhs4 = new Mahasiswa00("2504", "Naya", "1E", 4.0);
+        sll.print();
+        sll.addFirst(mhs4);
+        sll.print();
+        sll.addLast(mhs1);
+        sll.print();
+        sll.insertAfter("Alvaro", mhs3);
+        sll.insertAt(2, mhs2);
+        sll.print();
+        System.out.println("data index 1 : ");
+        sll.getData(1);
+        System.out.println("data mahasiswa an Alvaro berada pada index : " + sll.indexOf("Alvaro"));
+        System.out.println();
+        sll.removeFirst();
+        sll.removeLast();
+        sll.print();
+        sll.removeAt(0);
+        sll.print();
     }
 }
